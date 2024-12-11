f281c5ab2b2606f079dda6937a7b236d01f8cd62
Dion Mills <62540708+dionmills@users.noreply.github.com>
2024-12-11 00:38:22 +0000

    highlighted text block

---
+++ ESOAPIExplorer.Controls/BindableHighlighter.cs
@@ -0,0 +1,59 @@
+﻿using ESOAPIExplorer.Controls;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Documents;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using WinRT.Interop;
+using WinRT;
+
+namespace ESPAPIExplorer.Controls
+{
+    public sealed class BindableHighlighter : DependencyObject
+    {
+
+        public static readonly DependencyProperty BindableRangesProperty =
+        DependencyProperty.Register(nameof(BindableRanges), typeof(Dictionary<int,int>), typeof(BindableHighlighter), null);
+
+
+        public Dictionary<int, int> BindableRanges
+        {
+            get
+            {
+                return (Dictionary<int, int>)GetValue(BindableRangesProperty);
+            }
+            set
+            {
+                SetValue(BindableRangesProperty, value);
+                if (value != null)
+                {
+                    value.Select(r => new TextRange(r.Key, r.Value));
+                }
+                else
+                {
+                    SetValue(BindableRangesProperty, new List<TextRange>());
+                }
+            }
+        }
+
+        public static readonly DependencyProperty TextRangesProperty =
+        DependencyProperty.Register(nameof(TextRanges), typeof(List<TextRange>), typeof(BindableHighlighter), null);
+
+
+        public List<TextRange> TextRanges
+        {
+            get
+            {
+                return (List<TextRange>)GetValue(TextRangesProperty);
+            }
+            private set
+            {
+                SetValue(TextRangesProperty, value);
+            }
+        }
+    }
+}
