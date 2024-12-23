f02b5d85c9685f8a74d56cb70d211d7e8cf3e1a9
oakleya <agoakley@gmail.com>
2024-12-23 11:03:11 +0000

    add status

---
+++ ESOAPIExplorer.Models/StatusInformation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ESOAPIExplorer.Models;
+
+public class StatusInformation
+{
+    public int APIItems { get; set; }
+    public int FunctionItems { get; set; }
+    public int CFunctionItems { get; set; }
+    public int EnumTypes { get; set; }
+    public int EnumConstants { get; set; }
+    public int Globals { get; set; }
+}
